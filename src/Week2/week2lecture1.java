6bce5628f364a7243d42914999361b32b4117ec4
Finlay-McC <finlay.mccorrister@gmail.com>
2026-09-14 14:17:02 -0500

    Week 2 Lecture 1

---
+++ src/Week2/week2lecture1.java
@@ -0,0 +1,16 @@
+package Week2;
+
+import java.util.Scanner;
+
+public class week2lecture1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+        System.out.println("The square of " + num + " is " + (num * num));
+
+    }
+
+}
